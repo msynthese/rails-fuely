2ad28a8228c98a7644239f8a7dbac572bbe9fb8c
megret franck <msynthese@gmail.com>
2022-08-31 12:20:35 +0200

    Front sign et edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,15 +13,8 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # Overwriting the sign_up redirect path method
-  def after_sign_up_path_for(resource)
-    puts("enter in after-sign-in")
-    edit_user_registration_path
-  end
-
   # Overwriting the sign_ou redirect path method
   def after_sign_out_path_for(resource)
-    puts("enter in after-sign-out")
     root_path
   end
 end
