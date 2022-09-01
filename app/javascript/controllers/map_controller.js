5916a18c397a9457f64fda654cbf11f79c7aab23
megret franck <msynthese@gmail.com>
2022-09-01 16:25:37 +0200

    localisation stations

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,5 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+
 export default class extends Controller {
   static values = {
     apiKey: String,
@@ -8,20 +14,32 @@ export default class extends Controller {
 
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
-
+    navigator.geolocation.getCurrentPosition((pos) => this.#success(pos,this), this.#error, options)
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
-
+      style: "mapbox://styles/mapbox/streets-v10",
+      center: [12.550343, 55.665957],
+      zoom: 8
     })
-    // Create a default Marker and add it to the map.
-    const marker1 = new mapboxgl.Marker()
-    .setLngLat([12.554729, 55.70651])
-    .addTo(map);
-    this.#addMarkersToMap()
-    this.#fitMapToMarkers()
-    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl }))
+
+    // this.#fitMapToMarkers()
+    this.geocoder = new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl })
+
+    this.geocoder.on('result', (e) => {
+      const coord = e.result.geometry.coordinates
+      const lat = coord[1]
+      const lon = coord[0]
+
+      fetch(`/stations.json?lat=${lat}&lon=${lon}`,{ headers: {
+        'Content-Type': 'application/json'
+      }})
+      .then(data => data.json())
+      .then(data => this.#addMarkersToMap(data) )
+    })
+
+    this.#addMarkersToMap(this.markersValue)
+    this.map.addControl(this.geocoder)
   }
 
   #fitMapToMarkers() {
@@ -30,23 +48,37 @@ export default class extends Controller {
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 
-  #addMarkersToMap() {
-    this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window) // Add this
+  #addMarkersToMap(markers) {
+    markers.forEach((marker) => {
+     // const popup = new mapboxgl.Popup().setHTML(marker.info_window) // Add this
 
           // Create a HTML element for your custom marker
-      const customMarker = document.createElement("div")
-      customMarker.className = "marker"
-      customMarker.style.backgroundImage = `url('${marker.image_url}')`
-      customMarker.style.backgroundSize = "contain"
-      customMarker.style.width = "25px"
-      customMarker.style.height = "25px"
+      // const customMarker = document.createElement("div")
+      // customMarker.className = "marker"
+      // customMarker.style.backgroundImage = `url('${marker.image_url}')`
+      // customMarker.style.backgroundSize = "contain"
+      // customMarker.style.width = "25px"
+      // customMarker.style.height = "25px"
 
        // Pass the element as an argument to the new marker
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup) // Add this
+        //.setPopup(popup) // Add this
         .addTo(this.map)
     });
+  }
+
+  #success(pos, _this) {
+    const crd = pos.coords;
+
+    const center = new mapboxgl.LngLat(crd.longitude, crd.latitude);
+
+    // Center the map
+    // https://docs.mapbox.com/mapbox-gl-js/api/#map#setcenter
+    _this.map.setCenter(center);
+  }
+
+  #error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
   }
 }
