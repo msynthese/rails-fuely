50f2abb8faa6eebea07c02706769b156fbb6c294
Joao Henriques <joao33ti@gmail.com>
2022-09-05 15:41:56 +0200

    Add Brand to the Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name avatar fuel_preference])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name avatar fuel_preference brand_id])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update,
-                                    keys: %i[first_name last_name avatar fuel_preference])
+                                    keys: %i[first_name last_name avatar fuel_preference brand_id])
   end
 
   private
