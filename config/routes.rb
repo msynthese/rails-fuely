cb3e1e34b91df8a48cd215fdcf7f14aa7f2477ab
Joao Henriques <joao33ti@gmail.com>
2022-09-06 11:03:55 +0200

    Add Btn Brand and Corrections on ModelApplicationController

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'sessions', registrations: 'registrations' }
   root to: "pages#home"
   resources :locations, only: %i[index create]
-  resources :brands, only: %i[index create]
+  resources :brands, only: %i[index create edit update]
   resources :stations, only: %i[index show new]
   get "stations_map", to: "stations#index_map"
 end
