a0100fb49c60e28af3e053975571e5eb827630d7
Sofyen Ziadi <soz73456@gmail.com>
2022-09-06 09:17:01 +0200

    map-bubble-info WIP

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -8,6 +8,47 @@ class StationsController < ApplicationController
     @markers = []
     # The `geocoded` scope filters only flats with coordinates
     @markers = @stations.map do |station|
+    #   station_attr = {
+    #     coordinates: {
+    #       lat: 'XXX',
+    #       lng: 'XXX',
+    #     },
+    #     name: 'XXX',
+    #     brand: 'XXX',
+    #     address: 'XXX',
+    #     fuels: {
+    #       sp98: 'XXX',
+    #       sp95: 'XXX',
+    #     }
+    #   }
+
+    #   user_preferred_fuel_price =
+    #     case current_user_attr[:fuel_preference]
+    #     when "Gazole"
+    #         station["fields"]["price_gazole"]
+
+    #     when "SP98"
+    #         station["fields"]["price_sp98"]
+
+    #     when "SP95"
+    #       station["fields"]["price_sp95"]
+
+    #     when "E10"
+    #       station["fields"]["price_e10"]
+
+    #     when "E85"
+    #       station["fields"]["price_e85"]
+
+    #     when "GPLc"
+    #       station["fields"]["gplc"]
+
+    #     else
+    #         puts "Wrong action"
+    # end
+
+    #   else
+    #     nil
+    #   end
       {
         lat: station["geometry"]["coordinates"][1],
         lng: station["geometry"]["coordinates"][0],
@@ -20,6 +61,7 @@ class StationsController < ApplicationController
         sp98: station["fields"]["price_sp98"],
         sp95: station["fields"]["price_sp95"],
         e10: station["fields"]["price_e10"],
+        e85: station["fields"]["price_e85"], #did not find in API results yet
         gplc: station["fields"]["gplc"],
         fuel: station["fields"]["fuel"],
         shortage: station["fields"]["shortage"],
@@ -28,10 +70,11 @@ class StationsController < ApplicationController
         services: station["fields"]["services"],
         automate_24_24: station["fields"]["automate_24_24"],
         api_station_id: station["fields"]["id"],
-        info_window: render_to_string(partial: "info_window", locals: { station: }, formats: [:html])
+        info_window: render_to_string(partial: "info_window", locals: { station: }, formats: [:html]),
         # image_url: helpers.asset_url("REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS")
+        # STORE PREFERRED FUEL TYPE OF CURRENT USER
+        preferred_fuel: station["fields"]["price_gazole"]
       }
-
     end
 
 
