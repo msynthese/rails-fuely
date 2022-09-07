9ad85205ffec842a047f43aa795f6e6730152bc4
megret franck <msynthese@gmail.com>
2022-09-07 13:52:03 +0200

    blocage des icones map et favorite avant update

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
-  enum fuel_preference: %i[None SP95 SP98 E10 E85 Gazole GPLc Brand]
+  enum fuel_preference: %i[SP95 SP98 E10 E85 Gazole GPLc Brand]
+  enum brand_preference: %i[All Agip Auchan Avia Bp Casino Elan Esso_Express Intermarché Shell Système_U Total]
   has_many :locations, dependent: :destroy
   belongs_to :brand, optional: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :fuel_preference, presence: true, on: :update
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_05_093830) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_07_081837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_093830) do
     t.integer "fuel_preference"
     t.bigint "brand_id"
     t.integer "capacity"
+    t.integer "brand_preference"
     t.index ["brand_id"], name: "index_users_on_brand_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
